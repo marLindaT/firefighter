1672a0610a643443c82099535f1c9d862b50d52a
marLindaT <26019544+marLindaT@users.noreply.github.com>
2019-05-20 23:13:56 -0400

    Basic scraping accomplished
    Need to use regex to collect question options
    create list of dicts with keys: question, a,b,c,d,answer, (other superfluous options ie chapter, source, level...etc)

--- quizlet App/scraping.py
+++ quizlet App/scraping.py
@@ -1,5 +1,15 @@
+"""
+this is a WIP to get answers from quizlet page and make an iterable for JS on html app
+questions are provided in the same span with answer options from the raw html but are labelled with either A. to D.
+the correct answers are provided in separate spans(same class) but can be removed using basic modulus operator on list items
+
+trying to use regex to screen question_with_answer list by selecting options between A....D.
+
+final output should be a list of dicts (JS objects) for use in JS
 
 
+"""
+import re
 from bs4 import BeautifulSoup
 import os
 
@@ -11,27 +21,35 @@ import os
 
 soup = BeautifulSoup(open("firefighter1FinalTest_flashcards_sourceCode_html.html"), "html.parser")
 
-
-
     
-questions = []
-answers =[]
-correct_ans =[]
-
+raw_source = []
 
 
 for question in soup.find_all('span', {'class' : 'TermText notranslate lang-en'}):
     x = question.text.strip()
-    questions.append(x)
-    print(x)
+    raw_source.append(x)
+    #print(x)
+print(f"raw_source is {len(raw_source)} long")
 
-##for ans in soup.find_all('label', {'class' : 'm-l-1'}):
-##    x = ans.text.strip()
-##    answers.append(x)    
-##    #print(x)
-##    
-##for correct in soup.find_all('div', {'class' : 'rightanswer'}):
-##    x = correct.text.strip()
-##    correct_ans.append(x)
-##    #print(x)
+question_with_answers =[]
+
+for num in range(len(raw_source)):
+    if num%2==0:
+        question_with_answers.append(raw_source[num])
+
+answer_options =[]
+
+for item in question_with_answers:
+    m = re.search("?<=^[A-D]\.).+(?=[A-D]\.$", item)
         
+
+answers = []
+
+for num in range(len(raw_source)):
+    if num%2>0:
+        answers.append(raw_source[num])
+
+
+        
+
+
